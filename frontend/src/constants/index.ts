12b79d6b4e1fb37b5bcb19d6a8231cf054f0ae09
laios <1624456575@qq.com>
2024-10-04 20:20:01 +0800

    morph testnet

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -5,11 +5,18 @@ import { tokenAbi } from "./tokenAbi";
 import {bookingAbi} from "./bookingAbi"
 // const bookingAbi = BookingAbi.abi
 
+
+// anvil testnet
 // const tokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 // const bookingAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 
+// const tokenAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+// const bookingAddress = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
 
-const tokenAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
-const bookingAddress = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+
+// morph testnet
+const tokenAddress = "0xc4B232FE5204Db0A8A256A9b5a4691486B3Eb5A3";
+const bookingAddress = "0x686F99D09538192D212d0b788A575D7009325d09";
+
 
 export { tokenAbi , tokenAddress , bookingAbi , bookingAddress };
